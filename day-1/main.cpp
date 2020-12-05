15bdc6cad505ef6082a99a3050a727280dc4d5ca
klettari <klettari.k@gmail.com>
2020-12-05 11:02:51 -0500

    day 1, part 2

--- day-1/main.cpp
+++ day-1/main.cpp
@@ -1,3 +1,5 @@
+#define PART_TWO
+
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -5,6 +7,7 @@
 
 using namespace std;
 
+#ifdef PART_ONE
 int main()
 {
 	fstream f("input.txt", fstream::in);
@@ -46,3 +49,56 @@ int main()
 
 	return 0;
 }
+#endif // PART_ONE
+
+
+#ifdef PART_TWO
+int main()
+{
+	fstream f("input.txt", fstream::in);
+	const int shouldSumTo = 2020;
+
+	vector<int> numbers;
+	int first = 0;
+	int second = 0;
+	int third = 0;
+
+	bool getInput = true;
+
+	while (getInput)
+	{
+		f >> first;
+
+		if (f.eof())
+		{
+			getInput = false;
+		}
+
+		if (numbers.size() > 1)
+		{
+			for (int numberA : numbers)
+			{
+				for (int numberB : numbers)
+				{
+					if (first + numberA + numberB == shouldSumTo)
+					{
+						second = numberA;
+						third = numberB;
+						getInput = false;
+					}
+				}
+			}
+		}
+		numbers.push_back(first);
+	}
+
+	f.close();
+
+	cout << first << ", " << second << ", " << third << endl;
+
+	int multiplied = first * second * third;
+	cout << multiplied << endl;
+
+	return 0;
+}
+#endif // PART_TWO

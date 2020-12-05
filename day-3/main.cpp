30cd27d29f3cc38c0c0c1ee918768be504043b7a
klettari <klettari.k@gmail.com>
2020-12-05 12:21:02 -0500

    day 3, part 2

--- day-3/main.cpp
+++ day-3/main.cpp
@@ -1,4 +1,4 @@
-#define PART_ONE
+#define PART_TWO
 
 #include <fstream>
 #include <iostream>
@@ -60,3 +60,69 @@ int main()
 }
 
 #endif // PART_ONE
+
+#ifdef PART_TWO
+
+int main()
+{
+	fstream f("input.txt", fstream::in);
+
+	vector<string> lines;
+
+	bool hasInput = true;
+	while (hasInput)
+	{
+		string line;
+		f >> line;
+		lines.push_back(line);
+
+		if (f.eof())
+		{
+			hasInput = false;
+		}
+	}
+
+	f.close();
+
+	const vector<int> linesDown  { 1, 1, 1, 1, 2 };
+	const vector<int> linesRight { 1, 3, 5, 7, 1 };
+
+	const char tree = '#';
+	const char empty = '.';
+
+	vector<int> numberOfTrees;
+
+	for (int attempt = 0; attempt < linesDown.size(); attempt++)
+	{
+		int column = 0;
+		int numberOfTreesInAttempt = 0;
+		for (int row = 0; row < lines.size(); row += linesDown[attempt])
+		{
+			string line = lines[row];
+			char cell = line[column];
+			if (cell == tree)
+			{
+				numberOfTreesInAttempt++;
+			}
+
+			column += linesRight[attempt];
+			if (column >= line.size())
+			{
+				column %= line.size();
+			}
+		}
+
+		numberOfTrees.push_back(numberOfTreesInAttempt);
+		cout << numberOfTreesInAttempt << endl;
+	}
+
+	int multiplied = 1;
+	for (int number : numberOfTrees)
+	{
+		multiplied *= number;
+	}
+
+	cout << multiplied << endl;
+}
+
+#endif // PART_TWO
